29cf12551fd3729c582a154a7d42ad31e1721d19
dilshan-49 <bandararmtd.22@uom.lk>
2025-03-29 16:57:49 +0530

    calibration updated

--- colorCheck/calibration.py
+++ colorCheck/calibration.py
@@ -3,10 +3,41 @@ import numpy as np
 import csv
 import os
 
+from threading import Thread
+
+
+class Camera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            raise Exception("Error: Unable to access the camera.")
+        self.ret, self.frame = self.cap.read()
+        self.running = True
+        self.thread = Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        while self.running:
+            self.ret, self.frame = self.cap.read()
+
+    def get_frame(self):
+        if self.ret:
+            return self.ret,self.frame
+        return None
+    
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        self.cap.release()
+
+
+
 colorArray=["Blue","Red","Green","Yellow"]
 grab_area = [200,370,440,480]
+cam=Camera()
+
 def getframes(cap):
-        ret, frame = cap.read()
+        ret, frame = cam.get_frame()
         if ret:
             height, width, _ = frame.shape
             box_size = 70  # Size of the central box
